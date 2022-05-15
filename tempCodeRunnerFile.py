d14c9ff469d6d9bea12ae86062bf59b56a08cea9
Jason Chen <jasonchen870205@gmail.com>
2022-05-15 13:51:20 +0800

    0515

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    '''
-    # Construct covariance matrix
-    covarianceMatrix = []
-    for i in range(n):
-        covarianceMatrix.append([float(i) for i in input(f"Row {i}: ").split(' ')])
-    '''
